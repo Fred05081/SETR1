8c72546cc8ee0bd334c05a57be8f393d3434f73e
Pedrodrigues730 <pedro.m.rodrigues99@ua.pt>
2022-03-22 19:01:16 +0000

    My_fifoV1

--- assigment_1/MY_FIFO.c
+++ assigment_1/MY_FIFO.c
@@ -1,37 +1,59 @@
-#include "MY_FIFO.h"
+//#include "MY_FIFO.h"
+#include <stdio.h>
+#include <stdlib.h>
+#ifndef MAX_SIZE
+#define MAX_SIZE 5
+static int fifo_array[MAX_SIZE];
+static int head;
+static int tail;
+void MyFIFOInit(int tamanho)
+{
+    if (tamanho > MAX_SIZE)
+    {
+        tamanho = MAX_SIZE;
+    }
+    head = 0;
+    tail = 0;
+    for (int i = 0; i < tamanho; i++)
+    {
+        fifo_array[i] = 0;
+    }
+}
 
+void MyFIFOInsert(int add)
+{
+    fifo_array[head % MAX_SIZE] = add;
+    head++;
+}
 
-int MyFIFOInit(int tamanho)
+int MyFIFORemove(void)
 {
 
+    if (tail == head)
+    {
+        return -1;
+    }
 
-}                        
-
-int MyFIFOInsert(int add){
-
-
-
+    else
+    {
+        fifo_array[tail % MAX_SIZE] = 0;
+        tail++;
+    }
 }
 
-int MyFIFORemove(int rem){
+int MyFIFOPeep(void)
+{
+    int num;
 
+    num = fifo_array[head - 1];
 
-
-
+    return num;
 }
 
-int MyFIFOPeep(int look){
-
-
-
-
+int MyFIFOSize(void)
+{
+    int size;
+    size = head - tail;
+    printf("FIFO Size:  %d", size);
 }
-
-
-int MyFIFOSize(int tam){
-
-
-
-
-	
-}
+#endif
