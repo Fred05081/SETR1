cd5504beaf0140a3902cb76cda7f1b5c795ff1cb
Fred05081 <femanuelmm@ua.pt>
2022-03-22 17:57:49 +0000

    funçoes criadas

--- assigment_1/MY_FIFO.c
+++ assigment_1/MY_FIFO.c
@@ -1,1 +1,37 @@
-asd
+#include "MY_FIFO.h"
+
+
+int MyFIFOInit(int tamanho)
+{
+
+
+}                        
+
+int MyFIFOInsert(int add){
+
+
+
+}
+
+int MyFIFORemove(int rem){
+
+
+
+
+}
+
+int MyFIFOPeep(int look){
+
+
+
+
+}
+
+
+int MyFIFOSize(int tam){
+
+
+
+
+	
+}
--- assigment_1/test.c
+++ assigment_1/test.c
@@ -1,7 +1,34 @@
-main{
+/** @file app.c
+ * @brief app.c file brief decription 
+ *
+ * Follows the detailed description of app1.c. It is separated from 
+ * the brief one by a blank line.
+ * In this case app.c is the file that contains the main() function. 
+ * 
+ * @author Paulo Pedreiras
+ * @date 3 March 2022
+ * @bug No known bugs.
+ */
+
+/* Includes */
+#include <stdio.h>
+#include <stdlib.h>
+#include "MY_FIFO.h"
 
 
-
-
-return 0;
+/**
+ * @brief Brief decription of main().
+ *
+ * Here it goes the long description of main()
+ * main has no input arguments.
+ * It declares two variables and calls function1() using them 
+ * as argument. 
+ * It then prints the result and returns.
+ * 
+ * @return main() always returns 0
+ */
+int main(void)
+{
+	
+	return 0;
 }
